d5c53c80c2dcd8ff36374bc76b3b6029c4a8f081
Hossein-Falah <hosseinfalah2021@gmail.com>
2024-11-14 23:07:36 +0330

    feat: added register api endpoint in swagger

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -5,8 +5,11 @@ const registerValidation = Joi.object({
     firstname: Joi.string().required().min(2).max(20).error(createHttpError.BadRequest("نام باید بین 2 کاراکتر یا 20 کاراکتر باشد")),
     lastname: Joi.string().required().min(2).max(20).error(createHttpError.BadRequest("نام خانوادگی باید بین 2 کاراکتر یا 20 کاراکتر باشد")),
     email: Joi.string().email().required().error(createHttpError.BadRequest("ایمیل وارد شده صحیح نیست")),
-    phone: Joi.number().required().error(createHttpError.BadRequest("شماره موبایل باید 11 کاراکتر باشد")),
+    phone: Joi.alternatives().try(
+        Joi.number().integer().min(1000000000).max(99999999999)
+    ).required().error(createHttpError.BadRequest("شماره موبایل بدون وارد کن 0 و باید بین 10 تا 11 رقم باشد")),
     address: Joi.string().required().error(createHttpError.BadRequest("آدرس وارد شده صحیح نیست"))
+    
 });
 
 export {
