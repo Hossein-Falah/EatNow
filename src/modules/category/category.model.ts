0f9ba33198ec503330fe879d61d4aeef68141c3e
Hossein-Falah <hosseinfalah2021@gmail.com>
2024-11-29 09:19:46 +0330

    feat(category): Implement Category model with validations and image support

---
+++ src/modules/category/category.model.ts
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from "sequelize";
+import { ICategory } from "./category.interface";
+import sequelize from "../../config/db.config";
+
+export class Category extends Model<ICategory> implements ICategory {
+    declare id: string;
+    declare title: string;
+    declare images: string[]
+};
+
+Category.init({
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: { msg: "عنوان دسته بندی نمی تواند خالی باشد" },
+            len: { args: [3, 20], msg: "عنوان دسته بندی باید بین 3 تا 20 کاراکتر باشد" }
+        }
+    },
+    images: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize: sequelize,
+    tableName: "categories",
+    timestamps: true
+})
